bac8d871a24e8a7c82304d004fcfa45916acf961
symin0729 <symin0729@gmail.com>
2026-04-08 12:11:47 +0900

    JS 5일차 - 콘솔계산기

---
+++ fastapi/Console.js
@@ -0,0 +1,22 @@
+// 1. 숫자 2개를 변수로 선언함
+let a = 6;
+let b = 2;
+
+// 2. 연산자를 문자열로 정함("+", "-", "*", "/")
+let operator = "/"
+
+// 3. 연산자에 따라 계산 결과를 출력한다
+if (operator === "+") {
+  console.log(a + b);
+} else if (operator === "-") {
+  console.log(a - b);
+} else if (operator === "*") {
+  console.log(a * b);
+} else if (operator === "/") {
+  console.log(a / b);
+} else {
+  console.log("지원하지 않는 연산자입니다.");
+}
+
+
+
